059d1620ac13f3480640208913202887683dc06c
saintabomination <saint.abomination@protonmail.com>
2022-10-07 19:24:09 +0200

    Adding SFML

--- bake.sh
+++ bake.sh
@@ -3,8 +3,9 @@
 PROJECT_NAME="tetris-sfml"
 SRC_DIR="src"
 BUILD_DIR="build"
+LIBRARIES="-lsfml-graphics -lsfml-window -lsfml-system"
 
 mkdir -p $BUILD_DIR
 g++ -c $SRC_DIR/*.cpp
-g++ *.o -o $BUILD_DIR/$PROJECT_NAME
+g++ *.o -o $BUILD_DIR/$PROJECT_NAME $LIBRARIES
 rm *.o
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,40 @@
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
 #include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+const std::string WINDOW_TITLE = "SFML";
+const unsigned int WINDOW_MAX_FPS = 60;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
+  window.setFramerateLimit(WINDOW_MAX_FPS);
+  sf::CircleShape shape(25.f);
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      switch (event.type)
+      {
+        case sf::Event::Closed:
+          window.close();
+          break;
+
+        default:
+          break;
+      }
+
+      window.clear();
+      window.draw(shape);
+      window.display();
+    }
+  }
+
   return 0;
 }
